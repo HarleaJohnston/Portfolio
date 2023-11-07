c3a401048114c4a8c8f1bac9fe415a5c6f6061e4
Harlea R Johnston <98293078+HarleaJohnston@users.noreply.github.com>
2023-11-07 10:32:35 -0700

    Adding Fonts
    Adding Two fonts for the intro portion of my portfolio

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -8,7 +8,10 @@ function App() {
       <div className="webTop">
         <img className="imgSize" src={process.env.PUBLIC_URL + "/MyPic.png"}/>
         <div className="introBox">
-          <h1>Harlea Johnston</h1>
+          <h1 className="HF">Harlea Johnston</h1>
+          <h2 className="HF">(Any Pronouns)</h2>
+          <h3 className="HF2">Web Developer</h3>
+
         </div>
       </div>
     </div>
