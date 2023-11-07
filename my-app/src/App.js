4a7ce0cc094e430fad4c9cf7c6273aeaeb0fc3ce
Harlea R Johnston <hjohnston@student.neumont.edu>
2023-11-06 23:01:27 -0700

    Starting to Design
    Playing around with background imgs and my personal img of me. I am very picky when it comes to how I wish my site to look.

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,8 +4,13 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-
       </header>
+      <div className="webTop">
+        <img className="imgSize" src={process.env.PUBLIC_URL + "/MyPic.png"}/>
+        <div className="introBox">
+          <h1>Harlea Johnston</h1>
+        </div>
+      </div>
     </div>
   );
 }
