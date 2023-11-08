a216ca694a4b4b2bef5130a7f74800ff49b09204
Harlea R Johnston <98293078+HarleaJohnston@users.noreply.github.com>
2023-11-07 18:38:53 -0700

    Brute forcing it
    Forcing the text in my div to be on the background img I'm using for the intro box.  Will come back to make it do that with out brute forcing it in the future.

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -8,10 +8,22 @@ function App() {
       <div className="webTop">
         <img className="imgSize" src={process.env.PUBLIC_URL + "/MyPic.png"}/>
         <div className="introBox">
-          <h1 className="HF">Harlea Johnston</h1>
-          <h2 className="HF">(Any Pronouns)</h2>
-          <h3 className="HF2">Web Developer</h3>
-
+          <div className="introText">
+            <ul>
+              <h3 className="HF2">
+                <a href={"https://www.google.com/"} target="_blank" rel="noopener noreferrer">Email Me</a>
+              </h3>
+              <h1 className="HF">Harlea Johnston</h1>
+              <h2 className="HF">(Any Pronouns)</h2>
+              <h2 className="HF2">Web Developer</h2>
+              <div className="line"></div>
+              <h3 className="about">About Me:</h3>
+              <div className="aboutMeBox">
+                <p>ncdksmpnfkndskfnmkdsmkf dsmfmdls, nkdsfs dsmal;fm fmdsl;s</p>
+              </div>
+                <button>Resume</button>
+            </ul>
+          </div>
         </div>
       </div>
     </div>
