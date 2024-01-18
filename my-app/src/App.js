20ef33d6e57170a7a550ae6030833de2a2c645e5
Harlea R Johnston <98293078+HarleaJohnston@users.noreply.github.com>
2024-01-18 11:10:45 -0700

    Small Changes
    small changes

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -25,8 +25,6 @@ function App() {
 
       <AboutMe/>
 
-      <Skills/>
-
       <Projects/>
 
       <Education/>
--- my-app/src/Pages/Projects.js
+++ my-app/src/Pages/Projects.js
@@ -7,7 +7,7 @@ function Projects() {
     { id: 1, image: './imgs/CutTheBS.png', title: 'Cut the BS', description: `Site that gives users access to an recipe API. Cut the BS's goal to to provide users with recipes with out the stories that go with most online recipes.`, link: 'https://github.com/NovaGamer368/Cut-the-BS.git', proType: 'Group' },
     { id: 2, image: './imgs/UnhelpfulAI.png', title: 'Unhelpful AI', description: 'The Base of an Virtual Assistant. It can search Wikipedia, Tell Jokes, and Navigate to websites like Spotify.', link: 'https://github.com/HarleaJohnston/UnhelpfulAI.git', proType: 'Group' },
     { id: 3, image: './imgs/WO.png', title: 'Whendigo Occurrences', description: 'Whendigo Occurrences is an Blog ARG. This blog allows users to explore the site looking for clues as to what is happening in Whendigo, Missouri.', link: 'https://github.com/HarleaJohnston/Capstone_WhendigoOccurances', proType: 'Single' },
-    { id: 4, image: './imgs/WO.png', title: 'Annex', description: 'Is the Prototype of a library for Mult-Media Collectors. Is an library app that will help keep your collections in order and prevent spending money on things you already own', link: 'https://github.com/HarleaJohnston/Capstone_WhendigoOccurances', proType: 'Single' },
+    { id: 4, image: './imgs/3.png', title: 'Annex', description: `Is the Prototype of a library for Mult-Media Collectors. This library app's goal is to help keep your collections in order and prevent spending money on things you already own`, link: 'https://github.com/HarleaJohnston/Capstone_WhendigoOccurances', proType: 'Single' },
   ];
 
   return (
