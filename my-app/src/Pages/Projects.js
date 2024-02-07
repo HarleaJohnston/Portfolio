3df3795c43b4de2c261c96e1a4d22ebfe8e8848d
Harlea R Johnston <98293078+HarleaJohnston@users.noreply.github.com>
2024-02-07 15:50:21 -0700

    Fixed for projects for a more simple design.
    Didn't love the bootstrap carousel

--- my-app/src/Pages/Projects.js
+++ my-app/src/Pages/Projects.js
@@ -4,8 +4,10 @@ const Projects = () => {
   const data = [
     { id: 1, image: './imgs/CutTheBS.png', title: 'Cut the BS', description: `Site that gives users access to an recipe API. Cut the BS's goal to to provide users with recipes with out the stories that go with most online recipes.`, link: 'https://github.com/NovaGamer368/Cut-the-BS.git', proType: 'Group' },
     { id: 2, image: './imgs/UnhelpfulAI.png', title: 'Unhelpful AI', description: 'The Base of an Virtual Assistant. It can search Wikipedia, Tell Jokes, and Navigate to websites like Spotify.', link: 'https://github.com/HarleaJohnston/UnhelpfulAI.git', proType: 'Group' },
-    { id: 3, image: './imgs/WO.png', title: 'Whendigo Occurrences', description: 'Whendigo Occurrences is an Blog ARG. This blog allows users to explore the site looking for clues as to what is happening in Whendigo, Missouri.', link: 'https://github.com/HarleaJohnston/Capstone_WhendigoOccurances', proType: 'Single' },
-    { id: 4, image: './imgs/3.png', title: 'Annex', description: `Is the Prototype of a library for Mult-Media Collectors. This library app's goal is to help keep your collections in order and prevent spending money on things you already own`, link: 'https://github.com/HarleaJohnston/Capstone_WhendigoOccurances', proType: 'Single' },
+    { id: 3, image: './imgs/WO.png', title: 'Whendigo Occurrences', description: 'Whendigo Occurrences is an Blog ARG. This blog allows users to explore the site looking for clues as to what is happening in Whendigo, Missouri.', link: 'https://github.com/HarleaJohnston/Capstone_WhendigoOccurances', proType: 'Solo' },
+    { id: 4, image: './imgs/3.png', title: 'Annex', description: `Is the Prototype of a library for Mult-Media Collectors. This library app's goal is to help keep your collections in order and prevent spending money on things you already own`, link: 'https://github.com/HarleaJohnston/Capstone_WhendigoOccurances', proType: 'Group' },
+    { id: 5, image: './imgs/3.png', title: 'Add The BS', description: `Movie website with all the fixins `, link: 'https://github.com/HarleaJohnston/Capstone_WhendigoOccurances', proType: 'Group' },
+    { id: 6, image: './imgs/3.png', title: 'Annex: App', description: `Is a library for Mult-Media Collectors. This library app's goal is to help keep your collections in order and prevent spending money on things you already own`, link: 'https://github.com/HarleaJohnston/Capstone_WhendigoOccurances', proType: 'Group' },
   ];
 
   
@@ -26,7 +28,10 @@ const Projects = () => {
             <img src={card.image} className="card-img-top" alt="..." />
             <div className="card-body">
               <h5 className="card-title">{card.title}</h5>
+              <label>Project Type:</label>
+              <p className="card-text">{card.proType}</p>
               <p className="card-text">{card.description}</p>
+              <p className="card-text">{card.link}</p>
             </div>
           </div>
         ))}
